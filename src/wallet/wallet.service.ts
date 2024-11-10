f1cc41bec8ec6dcda935335f20e3ae8c8775acaa
vvvjey <hoangtu4520031234@gmail.com>
2024-11-10 21:26:57 +0700

    abc

--- src/wallet/wallet.service.ts
+++ src/wallet/wallet.service.ts
@@ -29,7 +29,7 @@ export class WalletService {
                 where: { id: userId },
             });
             if (!userExists) {
-                throw new Error('User does not exist'); 
+                throw new Error('User does not exist '); 
             }
             const wallet = await this.prismaService.wallet.create({
                 data:{
